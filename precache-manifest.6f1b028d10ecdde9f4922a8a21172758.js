b3662eb99c2b12e9cd19c2b74f0129768547a925
prashilbhimani <prashilbhimani24@gmail.com>
2020-04-06 00:47:27 -0600

    Updates

---
+++ precache-manifest.6f1b028d10ecdde9f4922a8a21172758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da213bcdcc17149192588b469a4ff5c",
+    "url": "/prashilbhimani.com/index.html"
+  },
+  {
+    "revision": "2532c79bfd26a3e60676",
+    "url": "/prashilbhimani.com/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8ebfeb1fc60ca29a6498",
+    "url": "/prashilbhimani.com/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "2532c79bfd26a3e60676",
+    "url": "/prashilbhimani.com/static/js/2.7338dfbe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/prashilbhimani.com/static/js/2.7338dfbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebfeb1fc60ca29a6498",
+    "url": "/prashilbhimani.com/static/js/main.ff6ef5f8.chunk.js"
+  },
+  {
+    "revision": "73a95e36c07a137f2263",
+    "url": "/prashilbhimani.com/static/js/runtime-main.975ada1c.js"
+  }
+]);
