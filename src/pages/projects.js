ef77c1b084014cc391d63c5becfe5bc5becd8026
prashilbhimani <prashilbhimani24@gmail.com>
2020-04-06 00:16:51 -0600

    Added initial files

---
+++ src/pages/projects.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+
+
+    render() {
+
+        return (
+            <div className="App">
+                <h1>List of Items</h1>
+                    <div>
+                        Project1
+                    </div>
+            </div>
+        );
+    }
+}
+
+export default Projects;
