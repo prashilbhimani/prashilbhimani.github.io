c46e2aca1fd9265c7f2751771a8af2ac13de6f64
prashilbhimani <prashilbhimani24@gmail.com>
2020-05-02 16:50:06 -0600

    Updates

---
+++ precache-manifest.ede79d03a6bb8d52b9f36e4e2e06f1f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ba95fdfb3eadc231396fdbfc475ca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2532c79bfd26a3e60676",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8ebfeb1fc60ca29a6498",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "2532c79bfd26a3e60676",
+    "url": "/static/js/2.7338dfbe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7338dfbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebfeb1fc60ca29a6498",
+    "url": "/static/js/main.ff6ef5f8.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
