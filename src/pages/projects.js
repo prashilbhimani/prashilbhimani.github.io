a159a21f82a315aac3ce3b0c156f98820b08f1c4
prashilbhimani <prashilbhimani24@gmail.com>
2020-05-02 22:58:08 -0600

    Added security tag

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
-
-
     render() {
-
         return (
             <div className="App">
                 <h1>List of Items</h1>
