c89a6f04b38931cd7eca98000a755510a1e732ab
prashilbhimani <prashilbhimani24@gmail.com>
2020-05-02 18:50:22 -0600

    Added the favicon in index.html

--- src/app/app.js
+++ src/app/app.js
@@ -9,7 +9,7 @@ class App extends Component {
             <div>
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route path="/projects" component={Projects} />
+                    <Route exact path="/projects" component={Projects} />
                 </Switch>
             </div>
         );
